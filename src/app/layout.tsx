4bdf7433b8fb1fb238d02f73f22506c10795525e
lukemauricejermy <luke@jermy.design>
2026-02-08 23:09:17 +0100

    default theme on page load listens to users system settings and theme on device

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,12 +16,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
           {children}
         </ThemeProvider>
       </body>
